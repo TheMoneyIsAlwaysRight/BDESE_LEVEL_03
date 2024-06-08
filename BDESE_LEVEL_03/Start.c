051cb54506594dff0052be54639a2a7a862e8b83
PAKJUNGMIN <vhzptvhdhfl@gmail.com>
2024-06-08 22:15:32 +0900

    초기 커밋.

---
+++ BDESE_LEVEL_03/Start.c
@@ -0,0 +1,21 @@
+/*
+	독학학위제 컴퓨터공학과 3단계 시험
+	
+	과목
+	0. 프로그래밍언어론(Programming language theory)
+	1. 인공지능(Artificial Intelligence)
+	2. 컴퓨터 네트워크 (Computer Network)
+	3. 소프트웨어공학(Software Engineering)
+	4. 정보보호(Information Security)
+	5. 임베디드시스템(Embedded System)
+
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("독학학위제 컴퓨터공학과 3단계 시험준비 메모용. ");
+
+	return 0;
+}
